ea9266e4ca3c953d1b17d327f65059604f53d6a1
Trieu Khang Trat <96287500+KayT256@users.noreply.github.com>
2024-03-16 10:44:39 -0500

    Split the script into 2 parts (main script and image_navigation)

--- script.js
+++ script.js
@@ -101,30 +101,3 @@ function removeSectionTransition() {
         section.style.transition = 'none'
     })
 }
-
-// Change the photos when click #next or #back
-document.addEventListener("DOMContentLoaded", () => {
-    var photos = document.querySelectorAll(".photo");
-    var currentIndex = 0;
-  
-    function changePhoto(index) {
-        photos.forEach((photo, i) => {
-            if (i === index) {
-                photo.classList.add("active");
-            } else {
-                photo.classList.remove("active");
-            }
-        });
-    }
-  
-    document.getElementById("next").addEventListener("click", () => {
-        currentIndex = (currentIndex + 1) % photos.length;
-        changePhoto(currentIndex);
-    });
-  
-    document.getElementById("back").addEventListener("click", () => {
-        currentIndex = (currentIndex - 1 + photos.length) % photos.length;
-        changePhoto(currentIndex);
-    });
-
-})
